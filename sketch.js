82f538e10b74a0a401f1da4971772829e4b20bfb
Ivancitop <93808733+Ivancitop@users.noreply.github.com>
2021-11-08 21:08:16 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -42,7 +42,7 @@ function setup() {
   trex.addAnimation("collided", trex_collided);
   trex.scale = 0.5;
   
-  ground = createSprite(width/2,height,width,2);
+  ground = createSprite(width/2,height,width,125);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -(6 + 3*score/100);
